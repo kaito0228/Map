15b243ce674f83b55a8418f04da2ab3378a65159
ryo.s <aqua.timez.saiko@gmail.com>
2019-04-27 18:00:02 +0900

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by ryo shirai on 2019/04/27.
+//  Copyright © 2019 ryo shirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
